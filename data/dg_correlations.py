46ae4c8bebd93cf5dbada2c31f8ca70fa6060e89
ih19708 <ih19708@bristol.ac.uk>
2022-03-29 18:47:50 +0100

    Remove CANOVA.

--- data/dg_correlations.py
+++ data/dg_correlations.py
@@ -1,15 +1,9 @@
-import os.path
-import pickle
-from typing import Tuple
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-from data.data_helpers import get_original_data, get_region_counts_df, get_weighted_mean_df
-from canova.src.canova.canova_source import canova
-from root import ROOT_DIR
+from data.data_helpers import get_weighted_mean_df
 
 
 def build_df() -> pd.DataFrame:
@@ -18,35 +12,20 @@ def build_df() -> pd.DataFrame:
     return df
 
 
-def canova_dependence(df: pd.DataFrame = None, file_path: str = None) -> Tuple[float, float]:
-    df = build_df() if df is None else df
-    dG_pairing = df["dG_pairing"]
-    dG_folding = df["dG_folding"]
-
-    file_path = file_path if file_path else f"{ROOT_DIR}/data/canova_results.pkl"
-    if os.path.exists(file_path):
-        with open(file_path, "rb") as f:
-            p_dG_pairing, p_dG_folding = pickle.load(f)
-
-    else:
-        p_dG_pairing = canova(dG_pairing, df["weighted_mean"])
-        p_dG_folding = canova(dG_folding, df["weighted_mean"])
-
-    print(f"p(dG_pairing) = {p_dG_pairing}")
-    print(f"p(dG_folding) = {p_dG_folding}")
-    return dG_pairing, dG_folding
-
-
-def plot_correlations_heatmap(df: pd.DataFrame = None) -> None:
+def plot_correlations_heatmap(df: pd.DataFrame = None, lower_only: bool = False,
+                              cmap: str = "vlag") -> None:
     df = build_df() if df is None else df
     corr = df.corr(method="spearman")
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)] = True
-    sns.heatmap(corr, cmap="vlag",   annot=True)
+    if lower_only:
+        sns.heatmap(corr, cmap=cmap, mask=mask, annot=False,)
+    else:
+        sns.heatmap(corr, cmap=cmap, annot=True)
+
     plt.show()
 
 
 if __name__ == "__main__":
     built_df = build_df()
     plot_correlations_heatmap(built_df)
-    canova_dependence(built_df)
